7e41726f59d710e1cc06e0f2b29717d36052a2ca
David Sankel <david@sankelsoftware.com>
2014-05-06 18:10:15 -0600

    Modified examples application to take an argument for which example to run.

--- examples/src/examples_main.cpp
+++ examples/src/examples_main.cpp
@@ -1,3 +1,4 @@
+#include <boost/optional.hpp>
 #include <QApplication>
 #include <QBrush>
 #include <QPen>
@@ -7,17 +8,18 @@
 #include <sani/interactiveanimationview.hpp>
 #include <sani/userinput.hpp>
 #include <sfrp/behaviormap.hpp>
-#include <sfrp/util.hpp>
+#include <sfrp/behaviorutil.hpp>
+#include <cstdlib>  // std::atoi
 #include <iostream>
 
-void setAnimation(sani::InteractiveAnimationView &animationView,
+void setAnimation(sani::InteractiveAnimationView& animationView,
                   sani::Animation animation) {
-  animationView.setInteractiveAnimation([animation](const sani::UserInput &) {
+  animationView.setInteractiveAnimation([animation](const sani::UserInput&) {
     return animation;
   });
 }
 
-void setMousePosAnimation(sani::InteractiveAnimationView &animationView,
+void setMousePosAnimation(sani::InteractiveAnimationView& animationView,
                           boost::function<sfrp::Behavior<sani::Drawing>(
                               sfrp::Behavior<QPointF>)> animation) {
   animationView.setInteractiveAnimation([animation](const sani::UserInput &
@@ -27,34 +29,68 @@ void setMousePosAnimation(sani::InteractiveAnimationView &animationView,
 }
 
 sani::Drawing circleAt(QPointF pos) {
-  return sani::drawEllipse(QPen(), QBrush(Qt::red),
-                           QRectF(-10 + pos.x(), -10 + pos.y(), 20, 20));
+  return sani::drawEllipse(
+      QPen(), QBrush(Qt::red), QRectF(-10 + pos.x(), -10 + pos.y(), 20, 20));
 }
 
 static void example1() {
   const sani::Animation circleAtCenter =
-      sfrp::pmConst(circleAt(QPointF(0.0, 0.0)));
+      sfrp::BehaviorUtil::always(circleAt(QPointF(0.0, 0.0)));
 
-  sani::InteractiveAnimationView * const view = new sani::InteractiveAnimationView();
+  sani::InteractiveAnimationView* const view =
+      new sani::InteractiveAnimationView();
   setAnimation(*view, circleAtCenter);
   view->show();
 }
 
-sfrp::Behavior<sani::Drawing>
-circleFollowsMouse(sfrp::Behavior<QPointF> mousePosition) {
+sfrp::Behavior<sani::Drawing> circleFollowsMouse(
+    sfrp::Behavior<QPointF> mousePosition) {
   return sfrp::BehaviorMap()(circleAt, mousePosition);
 }
 
 static void example2() {
-  sani::InteractiveAnimationView * const view = new sani::InteractiveAnimationView();
+  sani::InteractiveAnimationView* const view =
+      new sani::InteractiveAnimationView();
   setMousePosAnimation(*view, circleFollowsMouse);
   view->show();
 }
 
-int main(int argc, char **argv) {
-  QApplication app(argc, argv);
+// Parse the specified arguments (as passed to main) for a selected example
+// number. Return the example number (within [1..]) parsed or 'boost::none' if
+// there was a problem parsing the arguments.
+boost::optional<int> parseArgs(int argc, char** argv) {
+  if (argc != 2)
+    return boost::none;
+  else {
+    int parsedIntArg = std::atoi(argv[1]);
+    if (parsedIntArg >= 1)
+      return boost::make_optional(parsedIntArg);
+    else
+      return boost::none;
+  }
+}
 
-//  example1();
-  example2();
-  return app.exec();
+int main(int argc, char** argv) {
+
+  boost::optional<int> chosenExample = parseArgs(argc, argv);
+
+  if (chosenExample) {
+    QApplication app(argc, argv);
+
+    switch (*chosenExample) {
+      case 1:
+        example1();
+        break;
+      case 2:
+        example2();
+        break;
+      default:
+        example1();
+        break;
+    }
+    return app.exec();
+  } else {
+    std::cerr << "Usage: examples <ExampleNum>\n"
+              << "  where <ExampleNum> is a number 1 or greater.\n";
+  }
 }
