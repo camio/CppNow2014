dbd5b80a159eb8a0902f67649a29be895dba2f95
David Sankel <david@sankelsoftware.com>
2014-05-17 06:16:31 -0600

    Added more fp examples

--- fp/src/fp_main.cpp
+++ fp/src/fp_main.cpp
@@ -124,6 +124,96 @@ list<typename std::result_of<F(T)>::type> map2(F f, list<T> l) {
               l);
 }
 
+struct A{};
+struct B{};
+
+//struct AOrB {
+//  bool hasA;
+//  union {
+//    A a;
+//    B b;
+//  } contents;
+//};
+
+//struct AOrB {
+//  bool hasA;
+//  A a;
+//  B b;
+//};
+
+//struct AOrB {
+//  bool hasA();
+//  // Return the embedded 'A' object. The
+//  // behavior is undefined unless 'hasA()'.
+//  A getA();
+//  // Return the embedded 'B' object. The
+//  // behavior is undefined unless 'hasA()'.
+//  B getB();
+//  // post: 'hasA()'.
+//  void setA();
+//  // post: '!hasA()'.
+//  void setB();
+//private:
+//  //...
+//};
+
+struct AOrB {
+protected:
+  virtual void dummy(){}
+};
+
+struct AOrBWithA : AOrB {
+  A a;
+};
+struct AOrBWithB : AOrB {
+  B b;
+};
+struct Unit{};
+
+//typedef std::function< std::pair<Foo,int> (Unit) > Foo;
+
+//struct Foo {
+//  std::function< std::pair<Foo,int> (Unit) >
+//    function;
+//};
+
+//struct Foo
+//  : std::function< std::pair<int,Foo> (Unit) >
+//{
+//  template< typename F >
+//  Foo( F && f )
+//    : std::function< std::pair<int,Foo> (Unit) >(std::forward<F>(f))
+//  {}
+//};
+
+//struct Foo
+//  : std::function< std::pair<int,Foo> () >
+//{
+//  template< typename F >
+//  Foo( F && f )
+//    : std::function< std::pair<int,Foo> () >( std::forward<F>( f ) )
+//  {}
+//};
+
+//Foo foo = [](){ return std::make_pair(1, foo); };
+
+struct IntStream
+  : std::function< std::pair<int,IntStream> () >
+{
+  template< typename F >
+  IntStream( F && f )
+    : std::function< std::pair<int,IntStream> () >( std::forward<F>( f ) )
+  {}
+};
+
+std::pair<int,IntStream> naturalsFrom( int i ) {
+  return std::make_pair(
+    i,
+    std::bind( naturalsFrom, i+1 ) );
+}
+
+IntStream naturals = [](){ return naturalsFrom( 0 ); };
+
 int main()
 {
   list<int> l = addToFront(6, addToFront(5, addToFront(3, empty<int>())));
@@ -137,4 +227,10 @@ int main()
 
   std::cout << map2([](int i) { return "+" + std::to_string(i); }, l)
             << std::endl;
+
+//  std::cout << foo().first << std::endl;
+//  std::cout << foo().second().first << std::endl;
+  std::cout << naturals().first << std::endl;
+  std::cout << naturals().second().first << std::endl;
+  std::cout << naturals().second().second().first << std::endl;
 }
